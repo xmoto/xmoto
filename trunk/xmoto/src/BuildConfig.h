9a19474764d27bdcf43ce47b9a09ed16f44cf7ef
rneckelmann <rneckelmann@8dfcc510-c028-0410-813e-be6916f81dc3>
2006-05-19 18:59:17 +0000

    *** empty log message ***

--- trunk/xmoto/src/BuildConfig.h
+++ trunk/xmoto/src/BuildConfig.h
@@ -28,7 +28,7 @@ Info
 
 #define BUILD_MAJORVERSION    0
 #define BUILD_VERSION         1
-#define BUILD_MINORVERSION    13
+#define BUILD_MINORVERSION    14
 
 /*=============================================================================
 Build configuration
