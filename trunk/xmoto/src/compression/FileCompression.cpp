4655f1a714bba224d50a623130db1c8867b12b56
nadenislamarre <nadenislamarre@8dfcc510-c028-0410-813e-be6916f81dc3>
2006-07-07 20:31:09 +0000

    *** empty log message ***

--- trunk/xmoto/src/compression/FileCompression.cpp
+++ trunk/xmoto/src/compression/FileCompression.cpp
@@ -21,7 +21,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 #include "FileCompression.h"
 #include <bzlib.h>
-#include "VApp.h"
+#include "../VExcept.h"
 
 #define BUFSIZE 4096
 
