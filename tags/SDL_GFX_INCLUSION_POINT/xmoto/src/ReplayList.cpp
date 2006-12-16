0a75c1c5d96b981d6e46eaf5f139ece8191ce085
nobody <nobody@8dfcc510-c028-0410-813e-be6916f81dc3>
2006-12-16 09:07:30 +0000

    This commit was manufactured by cvs2svn to create tag 'SDL_GFX_INCLUSION_POINT'.

---
+++ tags/SDL_GFX_INCLUSION_POINT/xmoto/src/ReplayList.cpp
@@ -0,0 +1,172 @@
+/*=============================================================================
+XMOTO
+Copyright (C) 2005-2006 Rasmus Neckelmann (neckelmann@gmail.com)
+
+This file is part of XMOTO.
+
+XMOTO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+XMOTO is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with XMOTO; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+=============================================================================*/
+
+/* 
+ *  Replay list class
+ */
+#include "Replay.h"
+
+#define CURRENT_REPLAY_INDEX_FILE_VERSION 1
+
+namespace vapp {
+
+  void ReplayList::initFromCache() {
+    int v_nbReplays;
+    std::string v_replayName;
+
+    clear();
+
+    vapp::FileHandle *pfh = vapp::FS::openIFile(ReplayIndexFileName());
+    if(pfh == NULL) {
+      throw Exception((std::string("Unable to open file ") + ReplayIndexFileName()).c_str());
+    }
+
+    try {
+      int v_version = vapp::FS::readInt_LE(pfh); /* version */
+      if(v_version != CURRENT_REPLAY_INDEX_FILE_VERSION) {
+	throw Exception("Invalid replay index file version");
+      }
+      v_nbReplays = vapp::FS::readInt_LE(pfh);
+      for(int i=0; i<v_nbReplays; i++) {
+	try {
+	  v_replayName = vapp::FS::readString(pfh);
+	  addReplay(v_replayName, false);
+	} catch(Exception &e) {
+	}
+      }
+    } catch(Exception &e) {
+      clear();
+      vapp::FS::closeFile(pfh);
+      throw e;
+    }
+    vapp::FS::closeFile(pfh);
+  }
+
+  void ReplayList::saveCache() {
+    /* for windows : your must remove the file before create it */
+    remove(ReplayIndexFileName().c_str());
+    
+    FileHandle *pfh = FS::openOFile(ReplayIndexFileName());
+    if(pfh == NULL) {
+      throw Exception((std::string("Unable to open file ") + ReplayIndexFileName()).c_str());
+      return;
+    }
+    
+    try {
+      vapp::FS::writeInt_LE(pfh, CURRENT_REPLAY_INDEX_FILE_VERSION); /* version */
+      vapp::FS::writeInt_LE(pfh, m_Replays.size());
+      for(int i=0; i<m_Replays.size(); i++) {
+	vapp::FS::writeString(pfh, m_Replays[i]->Name);
+      }
+    } catch(Exception &e) {
+      vapp::FS::closeFile(pfh);
+      throw e;
+    }
+    vapp::FS::closeFile(pfh);
+  }
+
+  std::string ReplayList::ReplayIndexFileName() {
+    return vapp::FS::getUserDir() + "/" + "Replays/replays.index";
+  }
+
+  void ReplayList::initFromDir() {
+    this->clear();
+
+    std::vector<std::string> ReplayFiles;
+    ReplayFiles = FS::findPhysFiles("Replays/*.rpl");
+
+    for(unsigned int i=0; i<ReplayFiles.size(); i++) {
+      addReplay(FS::getFileBaseName(ReplayFiles[i]), false);
+    }
+    saveCache();
+  }
+
+  void ReplayList::addReplay(const std::string &Replay, bool i_saveCache) {
+    ReplayInfo* rplInfos;
+
+    if(FS::getFileBaseName(Replay) == "Latest") {
+      return;
+    }
+
+    /* Already got this replay? */
+    for(unsigned int i=0;i<m_Replays.size();i++) {
+      if(m_Replays[i]->Name == Replay) {
+	/* Yeah */
+	return;
+      }
+    }
+    
+    rplInfos = Replay::getReplayInfos(Replay);
+    if(rplInfos != NULL) {
+      m_Replays.push_back(rplInfos);
+    }
+
+    if(i_saveCache) {
+      saveCache();
+    }
+  }
+
+  void ReplayList::delReplay(const std::string &Replay, bool i_saveCache) {
+    for(unsigned int i=0;i<m_Replays.size();i++) {
+      if(m_Replays[i]->Name == Replay) {
+	delete m_Replays[i];
+	m_Replays.erase( m_Replays.begin() + i );
+	break;
+      }
+    }
+
+    if(i_saveCache) {
+      saveCache();
+    }
+  }
+
+  /*===========================================================================
+  Clean up stuff
+  ===========================================================================*/
+  void ReplayList::clear(bool i_saveCache) {
+    for(unsigned int i=0;i<m_Replays.size();i++) {
+      delete m_Replays[i];
+    }
+    m_Replays.clear();
+  }
+  
+  /*===========================================================================
+  Look up replays in list
+  ===========================================================================*/
+  std::vector<ReplayInfo *>* ReplayList::findReplays(const std::string &PlayerName,const std::string &LevelID) {
+    std::vector<ReplayInfo *> *Ret;
+    Ret = new std::vector<ReplayInfo *>;
+
+    /* find replays */
+    for(unsigned int i=0;i<m_Replays.size();i++) {
+      if((PlayerName == "" || PlayerName==m_Replays[i]->Player) &&
+         (LevelID == "" || LevelID==m_Replays[i]->Level)) {
+        /* Got one */
+        Ret->push_back(m_Replays[i]);
+      }
+    }
+    
+    /* Return list */
+    return Ret;
+  }
+
+}
+
---
+++ tags/SDL_GFX_INCLUSION_POINT/xmoto/src/VDraw.h
@@ -0,0 +1,76 @@
+/*=============================================================================
+XMOTO
+Copyright (C) 2005-2006 Rasmus Neckelmann (neckelmann@gmail.com)
+
+This file is part of XMOTO.
+
+XMOTO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+XMOTO is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with XMOTO; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+=============================================================================*/
+
+#ifndef __VDRAW_H__
+#define __VDRAW_H__
+
+#include "VCommon.h"
+
+#include "helpers/VMath.h"
+#include "VTexture.h"
+#include "Theme.h"
+
+namespace vapp {
+  /*===========================================================================
+  Class with various drawing functions
+  ===========================================================================*/
+  class DrawLib {
+    public:
+      DrawLib();
+
+      /* Methods - low-level */
+      void glVertex(float x,float y);
+      void screenProjVertex(float *x,float *y);      
+      
+      /* Methods - primitives */
+      void drawCircle(const Vector2f &Center,float fRadius,float fBorder=1.0f,Color Back=0,Color Front=-1);
+      void drawBox(const Vector2f &A,const Vector2f &B,float fBorder=1.0f,Color Back=0,Color Front=-1);      
+      void drawImage(const Vector2f &A,const Vector2f &B,Texture *pTexture,Color Tint=-1);
+      
+      /* Methods - text */
+      void drawText(const Vector2f &Pos,std::string Text,Color Back=0,Color Front=-1,bool bEdge=false);
+      int getTextWidth(std::string Text);
+      int getTextHeight(std::string Text);
+        
+    protected:
+    
+      /* Protected methods */
+      void setDrawDims(int nActualW,int nActualH,int w,int h) {m_nDrawWidth=w; m_nDrawHeight=h;
+                                                               m_nActualWidth=nActualW; m_nActualHeight=nActualH;}
+      void initLib(Theme *p_theme);
+      void uninitLib(Theme *p_theme);
+    
+    private:
+      /* Data */
+      int m_nDrawWidth,m_nDrawHeight;     
+      int m_nActualWidth,m_nActualHeight;     
+
+    Texture *m_pDefaultFontTex;
+      
+      /* Public helper methods */
+      void _InitTextRendering(Theme *p_theme);
+      void _UninitTextRendering(Theme *p_theme);
+  };
+  
+}
+
+#endif
+
