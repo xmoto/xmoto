5336d794ab42c337762ba10c1a2efb811265ee69
aspegic500 <aspegic500@8dfcc510-c028-0410-813e-be6916f81dc3>
2007-04-18 17:30:11 +0000

    svn version flag

--- trunk/src/svn_version.cpp
+++ trunk/src/svn_version.cpp
@@ -1,11 +1,5 @@
 #include <string>
 
 std::string svn_version() {
-  std::string v_rev;
-  v_rev = SVN_REV;
-
-  if(v_rev == "") {
-    v_rev = "$Revision: 1360 $";
-  }
-  return v_rev;
+  return SVN_REV;
 }
