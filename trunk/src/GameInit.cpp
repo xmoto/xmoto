0d6d16b83ff359368b3d27a6fc591b9c1fa5ceee
aspegic500 <aspegic500@8dfcc510-c028-0410-813e-be6916f81dc3>
2007-08-05 14:50:33 +0000

    update player name

--- trunk/src/GameInit.cpp
+++ trunk/src/GameInit.cpp
@@ -154,6 +154,18 @@ namespace vapp {
     if(m_sqlTrace) {
       m_db->setTrace(m_sqlTrace);
     }
+    /* set the room name ; set to WR if it cannot be determined */
+    m_WebHighscoresRoomName = "WR";
+    char **v_result;
+    int nrow;
+    v_result = m_db->readDB("SELECT name "
+			    "FROM webrooms "
+			    "WHERE id_room=" + m_WebHighscoresIdRoom + ";",
+			    nrow);
+    if(nrow == 1) {
+      m_WebHighscoresRoomName = m_db->getResult(v_result, 1, 0, 0);
+    }
+    m_db->read_DB_free(v_result);
 
     /* load theme */
     m_themeChoicer = new ThemeChoicer(
