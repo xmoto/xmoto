cc02b1ff97e62d18ed571f45910da854ed0fb130
theonlydude <theonlydude@8dfcc510-c028-0410-813e-be6916f81dc3>
2007-09-06 17:26:23 +0000

    new states

---
+++ src/states/StateDeadJust.cpp
@@ -0,0 +1,92 @@
+/*=============================================================================
+XMOTO
+
+This file is part of XMOTO.
+
+XMOTO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+XMOTO is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with XMOTO; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+=============================================================================*/
+
+#include "StateDeadJust.h"
+
+StateDeadJust::StateDeadJust(bool drawStateBehind,
+			     bool updateStatesBehind,
+			     GameApp* pGame):
+  GameState(drawStateBehind,
+	    updateStatesBehind,
+	    pGame)
+{
+
+}
+
+StateDeadJust::~StateDeadJust()
+{
+
+}
+
+
+void StateDeadJust::enter()
+{
+
+}
+
+void StateDeadJust::leave()
+{
+
+}
+
+void StateDeadJust::enterAfterPop()
+{
+
+}
+
+void StateDeadJust::leaveAfterPush()
+{
+
+}
+
+void StateDeadJust::update()
+{
+
+}
+
+void StateDeadJust::render()
+{
+
+}
+
+void StateDeadJust::keyDown(int nKey, SDLMod mod,int nChar)
+{
+
+}
+
+void StateDeadJust::keyUp(int nKey,   SDLMod mod)
+{
+
+}
+
+void StateDeadJust::mouseDown(int nButton)
+{
+
+}
+
+void StateDeadJust::mouseDoubleClick(int nButton)
+{
+
+}
+
+void StateDeadJust::mouseUp(int nButton)
+{
+
+}
---
+++ src/states/StateEditProfile.cpp
@@ -0,0 +1,92 @@
+/*=============================================================================
+XMOTO
+
+This file is part of XMOTO.
+
+XMOTO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+XMOTO is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with XMOTO; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+=============================================================================*/
+
+#include "StateEditProfile.h"
+
+StateEditProfile::StateEditProfile(bool drawStateBehind,
+				   bool updateStatesBehind,
+				   GameApp* pGame):
+  GameState(drawStateBehind,
+	    updateStatesBehind,
+	    pGame)
+{
+
+}
+
+StateEditProfile::~StateEditProfile()
+{
+
+}
+
+
+void StateEditProfile::enter()
+{
+
+}
+
+void StateEditProfile::leave()
+{
+
+}
+
+void StateEditProfile::enterAfterPop()
+{
+
+}
+
+void StateEditProfile::leaveAfterPush()
+{
+
+}
+
+void StateEditProfile::update()
+{
+
+}
+
+void StateEditProfile::render()
+{
+
+}
+
+void StateEditProfile::keyDown(int nKey, SDLMod mod,int nChar)
+{
+
+}
+
+void StateEditProfile::keyUp(int nKey,   SDLMod mod)
+{
+
+}
+
+void StateEditProfile::mouseDown(int nButton)
+{
+
+}
+
+void StateEditProfile::mouseDoubleClick(int nButton)
+{
+
+}
+
+void StateEditProfile::mouseUp(int nButton)
+{
+
+}
