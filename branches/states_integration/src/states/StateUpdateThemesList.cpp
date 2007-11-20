f7754e77ecac8dd185cdf5ed3f6c303801b99ca0
aspegic500 <aspegic500@8dfcc510-c028-0410-813e-be6916f81dc3>
2007-11-20 21:30:49 +0000

    themes list update

---
+++ branches/states_integration/src/states/StateUpdateThemesList.cpp
@@ -0,0 +1,87 @@
+/*=============================================================================
+XMOTO
+
+This file is part of XMOTO.
+
+XMOTO is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+XMOTO is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with XMOTO; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+=============================================================================*/
+
+#include "Game.h"
+#include "drawlib/DrawLib.h"
+#include "StateUpdateThemesList.h"
+#include "thread/UpdateThemesListThread.h"
+#include "helpers/Log.h"
+
+StateUpdateThemesList::StateUpdateThemesList(GameApp* pGame,
+				     bool drawStateBehind,
+				     bool updateStatesBehind)
+  : StateUpdate(pGame, drawStateBehind, updateStatesBehind)
+{
+  m_pThread          = new UpdateThemesListThread();
+  m_name             = "StateUpdateThemesList";
+}
+
+StateUpdateThemesList::~StateUpdateThemesList()
+{
+  delete m_pThread;
+}
+
+void StateUpdateThemesList::enter()
+{
+  StateUpdate::enter();
+
+  enterAttractMode(NO_KEY);
+}
+
+bool StateUpdateThemesList::update()
+{
+  if(StateUpdate::update() == false){
+    return false;
+  }
+
+  // thread finished. we leave the state.
+  if(m_threadStarted == true && m_pThread->isThreadRunning() == false){
+    m_pThread->waitForThreadEnd();
+    m_requestForEnd = true;
+    m_threadStarted = false;
+
+    Logger::Log("thread end");
+
+    return true;
+  }
+
+  if(m_threadStarted == false){
+    m_pThread->startThread(m_pGame);
+    m_threadStarted = true;
+
+    Logger::Log("thread started");
+  }
+
+  if(m_threadStarted == true){
+
+    // update the frame with the thread informations only when progress change
+    // to avoid spending tooo much time waiting for mutexes.
+    int progress = m_pThread->getThreadProgress();
+    if(progress != m_progress){
+      m_progress              = progress;
+      m_currentOperation      = m_pThread->getThreadCurrentOperation();
+      m_currentMicroOperation = m_pThread->getThreadCurrentMicroOperation();
+
+      updateGUI();
+    }
+  }
+
+  return true;
+}
