ca9a9a57cd2322dcf59a16e82b7f5dc24d7f9e37
aspegic500 <nicolas.adenis.lamarre@gmail.com>
2008-07-20 09:38:11 +0000

    FS#453 - add to favorite button enhancment

--- src/states/StateLevelPackViewer.h
+++ src/states/StateLevelPackViewer.h
@@ -52,7 +52,8 @@ private:
   static UIRoot* m_sGUI;
   static void createGUIIfNeeded();
   void updateGUI();
-    
+  void updateLevelsList();
+
   LevelsPack* m_pActiveLevelPack;
   bool m_require_updateLevelsList;
 
