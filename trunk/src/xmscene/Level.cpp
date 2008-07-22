7e6641b9e42ed6f96e627edffd9b8f8d3c2b319d
theonlydude <theonlydude@8dfcc510-c028-0410-813e-be6916f81dc3>
2008-07-22 15:46:19 +0000

    add block texture scale

--- trunk/src/xmscene/Level.cpp
+++ trunk/src/xmscene/Level.cpp
@@ -41,7 +41,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "ChipmunkWorld.h"
 #include "Theme.h"
 
-#define CACHE_LEVEL_FORMAT_VERSION 24
+#define CACHE_LEVEL_FORMAT_VERSION 25
 
 Level::Level() {
   m_xmotoTooOld = false;
