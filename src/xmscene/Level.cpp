4f05b208c5640c0c7b80197aa24511c769263e5d
aspegic500 <nicolas.adenis.lamarre@gmail.com>
2008-08-06 20:25:42 +0000

    fix chipmunk path

--- src/xmscene/Level.cpp
+++ src/xmscene/Level.cpp
@@ -37,7 +37,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "SkyApparence.h"
 #include "../Collision.h"
 #include "Scene.h"
-#include "../chipmunk.h"
+#include "../chipmunk/chipmunk.h"
 #include "ChipmunkWorld.h"
 #include "../Theme.h"
 
