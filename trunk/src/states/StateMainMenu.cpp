46f0df6ba8e73fb717125a66cc28f8c2e33439b2
aspegic500 <aspegic500@8dfcc510-c028-0410-813e-be6916f81dc3>
2008-08-07 20:54:20 +0000

    fix segfault bug about count updated - not finished

--- trunk/src/states/StateMainMenu.cpp
+++ trunk/src/states/StateMainMenu.cpp
@@ -1366,8 +1366,8 @@ void StateMainMenu::executeOneCommand(std::string cmd, std::string args)
 					   XMSession::instance()->idRoom(0),
 					   XMSession::instance()->debug(),
 					   xmDatabase::instance("main"));
-      updateLevelsPacksCountDetached();
       updateLevelsLists();
+      updateLevelsPacksCountDetached();
     } else
       m_require_updateLevelsList = true;
   } else {
