59d6a061cb6d06951c3f5c27f4e55b1c58233b57
aspegic500 <nicolas.adenis.lamarre@gmail.com>
2011-02-16 21:14:13 +0000

    some stats for the server

--- src/helpers/System.cpp
+++ src/helpers/System.cpp
@@ -23,6 +23,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "../include/xm_SDL.h"
 #include "Log.h"
 #include "VExcept.h"
+#include <sstream>
 
 std::vector<std::string>* System::getDisplayModes(int windowed) {
     std::vector<std::string>* modes = new std::vector<std::string>;
@@ -74,4 +75,72 @@ std::vector<std::string>* System::getDisplayModes(int windowed) {
     }
     
     return modes;
+}
+
+std::string System::getMemoryInfo() {
+  std::string v_res;
+
+#if defined(WIN32)
+  return "No available information";
+#else
+  // grep -E '^Vm' /proc/2532/status
+  //VmSize:	  403576 kB
+  //VmLck:	       0 kB
+  //VmRSS:	  140996 kB
+  //VmData:	  285376 kB
+  //VmStk:	     212 kB
+  //VmExe:	      48 kB
+  //VmLib:	   53016 kB
+  //VmPTE:	     508 kB
+  //VmSwap:	       0 kB
+
+  pid_t v_pid;
+  std::ostringstream v_pid_str;
+  std::string v_memfile;
+  FILE* fd;
+  char * v_line;
+  std::string v_str, v_var, v_value;
+  size_t v_len;
+  ssize_t v_read;
+  //size_t n;
+
+  v_pid = getpid();
+  v_pid_str << v_pid;
+  v_memfile = "/proc/" + v_pid_str.str() + "/status";
+
+  if( (fd = fopen(v_memfile.c_str(), "r")) == NULL) {
+    return "No available information";
+  }
+
+  v_line = NULL;
+  v_len = 0;
+  while ((v_read = getline(&v_line, &v_len, fd)) != -1) {
+    v_str = v_line;
+    if(v_str.substr(0, 2) == "Vm") {
+      v_res += v_str;
+    }
+    
+    //    // search the : delimiter
+    //    if( (n = v_str.find(":")) != std::string::npos) {
+    //      v_var   = v_str.substr(0, n);
+    //      n++; // to remove the :
+    //      // remove unnecessary spaces used in this file
+    //      while(n < v_str.size() && (v_str[n] == '\t' || v_str[n] == ' ')) {
+    //	n++;
+    //      }
+    //      if(n < v_str.size()) {
+    //	// a string and a non empty value is found
+    //	v_value = v_str.substr(n, v_str.length() - n -1 /* remove the \n */);
+    //	printf("+var=%s+val=%s+\n", v_var.c_str(), v_value.c_str());
+    //      }
+    //    }
+  }
+  if(v_line != NULL) {
+    free(v_line);
+  }
+
+  fclose(fd);
+#endif
+
+  return v_res;
 }
