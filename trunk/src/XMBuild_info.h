0ebc8a55320da84d143d4e21b10b901c54f203d7
aspegic500 <aspegic500@8dfcc510-c028-0410-813e-be6916f81dc3>
2011-04-08 19:51:55 +0000

    to 0.5.7

--- trunk/src/XMBuild_info.h
+++ trunk/src/XMBuild_info.h
@@ -16,5 +16,5 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #define BUILD_MAJORVERSION    0
 #define BUILD_VERSION         5
 #define BUILD_MINORVERSION    7
-#define BUILD_EXTRAINFO       "svn"
+#define BUILD_EXTRAINFO       ""
 #define BUILD_COPYRIGHT       "(C) Copyright 2005-2011"
