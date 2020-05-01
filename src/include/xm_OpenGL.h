f0cad0600aad6770101828733efda6e337bea143
Stephen Kitt <steve@sk2.org>
2020-05-01 14:48:18 +0200

    Stop shipping glext.h
    The version of glext.h included with X-Moto is licensed under SGI Free
    Software License B, version 1.1, which is considered non-free at least
    by the Debian project (see https://bugs.debian.org/368560 for
    details).
    
    Removing glext.h avoids this problem, and ensures the build agrees
    with the platform's GL extensions. It also avoids potentially
    conflicting with the platform's glext.h; on current OpenGL versions,
    the latter is protected by __gl_glext_h_, not __glext_h_ as defined in
    xm_OpenGL.h, so it ends up being included anyway.
    
    Signed-off-by: Stephen Kitt <steve@sk2.org>

--- src/include/xm_OpenGL.h
+++ src/include/xm_OpenGL.h
@@ -1,7 +1,6 @@
 #ifdef ENABLE_OPENGL
 /* Pull in OpenGL headers */
 /* following scissored from SDL_opengl.h */
-#define __glext_h_ /* Don't let gl.h include glext.h */
 #ifdef HAVE_APPLE_OPENGL_FRAMEWORK
 #include <OpenGL/gl.h> /* Header File For The OpenGL Library */
 #include <OpenGL/glu.h> /* Header File For The GLU Library */
@@ -12,8 +11,5 @@
 #include <GL/gl.h> /* Header File For The OpenGL Library */
 #include <GL/glu.h> /* Header File For The GLU Library */
 #endif
-#undef __glext_h_
-
-#include "xmoto/glext.h"
 
 #endif
