14145244fabd8d4bccbb9ec5ff31dd76931c0caa
_yui <imbatman0xff@gmail.com>
2020-06-16 21:21:00 +0300

    Fix incorrect brew deps

--- .travis/macos/deps.sh
+++ .travis/macos/deps.sh
@@ -3,6 +3,13 @@
 set -ex
 
 brew update
-brew install \
-  cmake ninja jpeg libpng gettext sdl sdl_mixer \
-  sdl_net sdl_ttf bz2 curl libxml2 xz zlib
+
+# packages to be checked for before installing them
+for pkg in cmake ninja jpeg libpng \
+           bzip2 curl sqlite3 libxml2 zlib; do
+  brew list "$pkg" &>/dev/null || brew install "$pkg"
+done
+
+# install the rest of the packages
+brew install gettext sdl sdl_mixer sdl_net sdl_ttf
+
