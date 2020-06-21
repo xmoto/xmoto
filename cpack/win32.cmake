xmoto/xmoto

commit 4bfa69773998a028c9bc840cb17db187c58e19c6

Add win32 cross-builds to travis

cpack/win32.cmake
@@ -1,12 +1,11 @@
 if(WIN32)
-  set(CPACK_GENERATOR "ZIP;NSIS")
+  #set(CPACK_GENERATOR "ZIP;NSIS")
 
   if(CPACK_GENERATOR MATCHES "NSIS")
     set(CPACK_NSIS_MUI_ICON "${CMAKE_SOURCE_DIR}/extra/xmoto_icone.ico")
     set(CPACK_NSIS_MUI_UNIICON "${CPACK_NSIS_MUI_ICON}")
     set(CPACK_NSIS_EXECUTABLES_DIRECTORY "")
     set(CPACK_INCLUDE_TOPLEVEL_DIRECTORY FALSE)
-    set(CPACK_PACKAGE_FILE_NAME "${CPACK_PACKAGE_FILE_NAME}-setup")
 
     set(CPACK_NSIS_CREATE_ICONS_EXTRA
       "CreateShortCut \\\"$DESKTOP\\\\X-Moto.lnk\\\" \\\"$INSTDIR\\\\xmoto.exe\\\""

.travis.yml
@@ -8,17 +8,23 @@ matrix:
       sudo: required
       install: ".travis/linux/deps.sh"
       script: ".travis/linux/build.sh"
+
     - os: osx
       sudo: required # see the build script...
       osx_image: xcode10.1 # =10.13/high sierra
       install: ".travis/macos/deps.sh"
       script: ".travis/macos/build.sh"
+      cache:
+        directories:
+          - $HOME/Library/Caches/Homebrew
+      before_cache:
+        - brew cleanup
 
-cache:
-  directories:
-    - $HOME/Library/Caches/Homebrew
-before_cache:
-  - brew cleanup
+    - os: linux
+      dist: bionic
+      name: "Linux MXE"
+      install: ".travis/linux-mxe/deps.sh"
+      script: ".travis/linux-mxe/build.sh"
 
 deploy:
   provider: releases
@@ -28,8 +34,10 @@ deploy:
     - 'xmoto-0.6.0.deb'
     - 'xmoto-0.6.0.rpm'
     - 'xmoto-0.6.0.dmg'
+    - 'xmoto-0.6.0-setup.exe'
+    - 'xmoto-0.6.0-win32.zip'
   skip_cleanup: true
   on:
-    branch: "release"
+    branch: "prep-for-release"
     tags: true
 

.travis/linux-mxe/build.sh
@@ -1,0 +1,88 @@
+#!/bin/sh
+
+set -ex
+
+# needed for cmake to find some programs
+export PATH="/usr/lib/mxe/usr/bin:$PATH"
+
+mkdir build && cd build
+mkdir -p /tmp/xmoto
+
+MXE_PATH=/usr/lib/mxe
+MXE_TARGET="i686-w64-mingw32.shared"
+
+is_broken_symlink() {
+  if [ -L "$1" ] && [ ! -e "$1" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+relink() {
+  for prog in "$@"; do
+    if is_broken_symlink "$MXE_HOST_PFX-$prog"; then
+      sudo rm "$MXE_HOST_PFX-$prog"
+      sudo ln -s "$MXE_CROSS_PFX-$prog" "$MXE_HOST_PFX-$prog"
+    fi
+  done
+}
+
+MXE_HOST_PFX="$MXE_PATH/usr/x86_64-pc-linux-gnu/bin/$MXE_TARGET"
+MXE_CROSS_PFX="$MXE_PATH/usr/bin/$MXE_TARGET"
+
+# add the mxe DLLs to wine's path
+export WINEPATH="$MXE_PATH/usr/$MXE_TARGET/bin${WINEPATH:+:${WINEPATH}}"
+export WINEARCH=win32
+
+echo "MXE_PATH: $MXE_PATH"
+echo "MXE HOST PREFIX: $MXE_HOST_PFX"
+echo "MXE CROSS PREFIX: $MXE_CROSS_PFX"
+echo "WINEPATH: $WINEPATH"
+
+relink "gcc" "g++"
+
+"$MXE_CROSS_PFX-cmake" \
+  -DCMAKE_INSTALL_PREFIX=/tmp/xmoto \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DXMOTO_PACK=manual ..
+
+make -j"$(nproc)"
+
+Xvfb :0 -screen 0 1024x768x16 &
+xvfb_pid=$!
+
+sleep 5
+
+echo "pidof Xvfb:"
+pidof Xvfb
+
+make xmoto_pack
+
+# not that any reasonable PID 1 responds to SIGTERM anyway ;)
+if [ "$xvfb_pid" -gt 1 ]; then
+  kill -15 "$xvfb_pid" || true
+fi
+
+# aaand if the graceful termination doesn't work..
+jobs -p | xargs -r kill -9
+
+#make install
+
+"$MXE_CROSS_PFX"-cpack -G "NSIS"
+"$MXE_CROSS_PFX"-cpack -G "ZIP"
+
+fix_pkg_name() {
+  if [ -z "$1" ] || [ -z "$2" ]; then
+    >&2 echo "Error: [fix_pkg_name]: 2 parameters required"
+  fi
+  file="$1"
+  ext="${file##*.}"
+  mv "$file" "${file%.$ext}-$2${ext:+.${ext}}"
+}
+
+# add -win32 to .zip files, and -setup to .exe files
+for pkg in xmoto-*.zip; do fix_pkg_name "$pkg" "win32"; done
+for pkg in xmoto-*.exe; do fix_pkg_name "$pkg" "setup"; done
+
+

.travis/linux-mxe/deps.sh
@@ -1,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 86B72ED9
+sudo add-apt-repository 'deb [arch=amd64] http://mirror.mxe.cc/repos/apt bionic main'
+
+# for xvfb
+sudo add-apt-repository universe
+
+sudo apt-get update
+
+sudo apt-get install -y \
+  mxe-i686-w64-mingw32.shared-{cc,curl,jpeg,libpng,zlib,libxml2,sqlite,gettext} \
+  mxe-i686-w64-mingw32.shared-{smpeg2,sdl,sdl-mixer,sdl-net,sdl-ttf,lzma,bzip2,nsis} \
+  wine-stable wine32 xvfb
+

bin/CMakeLists.txt
@@ -3,7 +3,11 @@ set_property(CACHE XMOTO_PACK PROPERTY STRINGS "manual" "always")
 
 add_custom_command(
   OUTPUT xmoto.bin
-  COMMAND xmoto --pack xmoto.bin "${CMAKE_CURRENT_SOURCE_DIR}"
+  #COMMAND xmoto --pack xmoto.bin "${CMAKE_CURRENT_SOURCE_DIR}"
+  #COMMAND echo "PROJECT_BINARY_DIR: ${PROJECT_BINARY_DIR}\nCMAKE_CURRENT_SOURCE_DIR: ${CMAKE_CURRENT_SOURCE_DIR}"
+  #COMMAND whereis wine
+  #COMMAND bash -c wine
+  COMMAND ${CMAKE_COMMAND} -E env DISPLAY=:0.0 wine "${PROJECT_BINARY_DIR}/src/xmoto.exe" --pack xmoto.bin "${CMAKE_CURRENT_SOURCE_DIR}"
   MAIN_DEPENDENCY package.lst
   COMMENT "Packing xmoto.bin"
 )

tools/win32_copy_dlls.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/sh
+#!/bin/sh
 
 is_mxe=0
 mxe_compiler_suffix="-gcc"

tools/win32_strip_dlls.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/sh
+#!/bin/sh
 
 # A simple script to `strip` all DLL files in the specified folder
 

commit 333abe273ae75da82ad24bc0e20efc404b55ccfd

CMake: Fix regression with NSIS installer icons
Commit 4bfa697 introduced a regression that would cause the NSIS installer to not add icons properly, and to not create desktop icons/start menu entries

cpack/win32.cmake
@@ -1,18 +1,16 @@
 if(WIN32)
-  #set(CPACK_GENERATOR "ZIP;NSIS")
+  set(CPACK_NSIS_MUI_ICON "${CMAKE_SOURCE_DIR}/extra/xmoto_icone.ico")
+  set(CPACK_NSIS_MUI_UNIICON "${CPACK_NSIS_MUI_ICON}")
+  set(CPACK_NSIS_EXECUTABLES_DIRECTORY "")
+  set(CPACK_INCLUDE_TOPLEVEL_DIRECTORY FALSE)
 
-  if(CPACK_GENERATOR MATCHES "NSIS")
-    set(CPACK_NSIS_MUI_ICON "${CMAKE_SOURCE_DIR}/extra/xmoto_icone.ico")
-    set(CPACK_NSIS_MUI_UNIICON "${CPACK_NSIS_MUI_ICON}")
-    set(CPACK_NSIS_EXECUTABLES_DIRECTORY "")
-    set(CPACK_INCLUDE_TOPLEVEL_DIRECTORY FALSE)
+  set(CPACK_NSIS_CREATE_ICONS_EXTRA
+    "CreateShortCut \\\"$DESKTOP\\\\X-Moto.lnk\\\" \\\"$INSTDIR\\\\xmoto.exe\\\""
+    "CreateShortCut \\\"$STARTMENU_FOLDER\\\\X-Moto.lnk\\\" \\\"$INSTDIR\\\\xmoto.exe\\\""
+  )
 
-    set(CPACK_NSIS_CREATE_ICONS_EXTRA
-      "CreateShortCut \\\"$DESKTOP\\\\X-Moto.lnk\\\" \\\"$INSTDIR\\\\xmoto.exe\\\""
-      "CreateShortCut \\\"$STARTMENU_FOLDER\\\\X-Moto.lnk\\\" \\\"$INSTDIR\\\\xmoto.exe\\\""
-    )
-  else()
-    set(CPACK_INCLUDE_TOPLEVEL_DIRECTORY TRUE)
-  endif()
+  # cpack will complain if this is set for the NSIS generator,
+  # but there doesn't seem to be any way to conditionally set it...
+  set(CPACK_INCLUDE_TOPLEVEL_DIRECTORY TRUE)
 endif()
 
333abe273ae75da82ad24bc0e20efc404b55ccfd
_yui <imbatman0xff@gmail.com>
2020-06-21 21:03:02 +0300

    CMake: Fix regression with NSIS installer icons
    Commit 4bfa697 introduced a regression that would cause the NSIS installer to not add icons properly, and to not create desktop icons/start menu entries

--- cpack/win32.cmake
+++ cpack/win32.cmake
@@ -1,18 +1,16 @@
 if(WIN32)
-  #set(CPACK_GENERATOR "ZIP;NSIS")
+  set(CPACK_NSIS_MUI_ICON "${CMAKE_SOURCE_DIR}/extra/xmoto_icone.ico")
+  set(CPACK_NSIS_MUI_UNIICON "${CPACK_NSIS_MUI_ICON}")
+  set(CPACK_NSIS_EXECUTABLES_DIRECTORY "")
+  set(CPACK_INCLUDE_TOPLEVEL_DIRECTORY FALSE)
 
-  if(CPACK_GENERATOR MATCHES "NSIS")
-    set(CPACK_NSIS_MUI_ICON "${CMAKE_SOURCE_DIR}/extra/xmoto_icone.ico")
-    set(CPACK_NSIS_MUI_UNIICON "${CPACK_NSIS_MUI_ICON}")
-    set(CPACK_NSIS_EXECUTABLES_DIRECTORY "")
-    set(CPACK_INCLUDE_TOPLEVEL_DIRECTORY FALSE)
+  set(CPACK_NSIS_CREATE_ICONS_EXTRA
+    "CreateShortCut \\\"$DESKTOP\\\\X-Moto.lnk\\\" \\\"$INSTDIR\\\\xmoto.exe\\\""
+    "CreateShortCut \\\"$STARTMENU_FOLDER\\\\X-Moto.lnk\\\" \\\"$INSTDIR\\\\xmoto.exe\\\""
+  )
 
-    set(CPACK_NSIS_CREATE_ICONS_EXTRA
-      "CreateShortCut \\\"$DESKTOP\\\\X-Moto.lnk\\\" \\\"$INSTDIR\\\\xmoto.exe\\\""
-      "CreateShortCut \\\"$STARTMENU_FOLDER\\\\X-Moto.lnk\\\" \\\"$INSTDIR\\\\xmoto.exe\\\""
-    )
-  else()
-    set(CPACK_INCLUDE_TOPLEVEL_DIRECTORY TRUE)
-  endif()
+  # cpack will complain if this is set for the NSIS generator,
+  # but there doesn't seem to be any way to conditionally set it...
+  set(CPACK_INCLUDE_TOPLEVEL_DIRECTORY TRUE)
 endif()
 
