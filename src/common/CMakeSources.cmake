d188a03252d3fcc4884b09749e92cc89aa9a9ede
Dusk Banks <me@bb010g.com>
2020-07-29 01:08:32 -0700

    CMake: Consistently format CMakeSources

--- src/common/CMakeSources.cmake
+++ src/common/CMakeSources.cmake
@@ -1,46 +1,28 @@
 target_sources_relative(xmoto PRIVATE
   ./BuildConfig.h
-  ./CRCHash.cpp
-  ./CRCHash.h
-  ./CameraAnimation.cpp
-  ./CameraAnimation.h
-  ./DBuffer.cpp
-  ./DBuffer.h
-  ./Image.cpp
-  ./Image.h
+  ./CRCHash.cpp ./CRCHash.h
+  ./CameraAnimation.cpp ./CameraAnimation.h
+  ./DBuffer.cpp ./DBuffer.h
+  ./Image.cpp ./Image.h
   ./Languages.h
-  ./Locales.cpp
-  ./Locales.h
-  ./Packager.cpp
-  ./Packager.h
-  ./PolyDraw.cpp
-  ./PolyDraw.h
-  ./Theme.cpp
-  ./Theme.h
-  ./VBezier.cpp
-  ./VBezier.h
+  ./Locales.cpp ./Locales.h
+  ./Packager.cpp ./Packager.h
+  ./PolyDraw.cpp ./PolyDraw.h
+  ./Theme.cpp ./Theme.h
+  ./VBezier.cpp ./VBezier.h
   ./VCommon.h
-  ./VFileIO.cpp
-  ./VFileIO.h
+  ./VFileIO.cpp ./VFileIO.h
   ./VFileIO_types.h
-  ./VTexture.cpp
-  ./VTexture.h
-  ./VXml.cpp
-  ./VXml.h
-  ./WWW.cpp
-  ./WWW.h
+  ./VTexture.cpp ./VTexture.h
+  ./VXml.cpp ./VXml.h
+  ./WWW.cpp ./WWW.h
   ./WWWAppInterface.h
-  ./XMArgs.cpp
-  ./XMArgs.h
-  ./XMBuild.cpp
-  ./XMBuild.h
+  ./XMArgs.cpp ./XMArgs.h
+  ./XMBuild.cpp ./XMBuild.h
   ./XMBuild_info.h
-  ./XMSession.cpp
-  ./XMSession.h
-  ./XMSession_default.cpp
-  ./XMSession_default.h
+  ./XMSession.cpp ./XMSession.h
+  ./XMSession_default.cpp ./XMSession_default.h
   ./XMotoLoadLevelsInterface.h
   ./XMotoLoadReplaysInterface.h
-  ./svn_version.cpp
-  ./svn_version.h
+  ./svn_version.cpp ./svn_version.h
 )
