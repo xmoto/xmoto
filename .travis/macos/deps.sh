0e017a8759a977662731c757f7786bb4cc993a02
_yui <imbatman0xff@gmail.com>
2021-02-17 00:23:51 +0200

    Travis: Add ccache

--- .travis/macos/deps.sh
+++ .travis/macos/deps.sh
@@ -6,7 +6,7 @@ export HOMEBREW_NO_AUTO_UPDATE=1
 export HOMEBREW_NO_ANALYTICS=1
 
 # packages to be checked for before installing them
-for pkg in cmake ninja jpeg libpng \
+for pkg in cmake ninja ccache jpeg libpng \
            bzip2 curl sqlite3 libxml2 zlib; do
   brew list "$pkg" &>/dev/null || brew install "$pkg"
 done
