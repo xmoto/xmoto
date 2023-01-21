2a57e645bf4dbd619efbf3f9bbd5be634daa16e2
_yui <imbatman0xff@gmail.com>
2023-01-21 13:00:18 +0200

    Use explicit ISO 8601 format with std::strftime
    https://sourceforge.net/p/mingw-w64/bugs/793/

--- src/helpers/Time.cpp
+++ src/helpers/Time.cpp
@@ -4,6 +4,6 @@
 std::string iso8601Date() {
   std::time_t t = std::time(nullptr);
   char datetime[sizeof("1970-01-01T00:00:00Z")];
-  std::strftime(datetime, sizeof(datetime), "%FT%TZ", std::localtime(&t));
+  std::strftime(datetime, sizeof(datetime), "%Y-%m-%dT%H:%M:%SZ", std::localtime(&t));
   return std::string(datetime);
 }
