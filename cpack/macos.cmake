7a7a2ebf9fdec0e306e5ae2e4abb1024f9a0877b
_yui <imbatman0xff@gmail.com>
2023-02-26 22:43:31 +0200

    CMake: Tidy up macOS bundles

--- cpack/macos.cmake
+++ cpack/macos.cmake
@@ -1,9 +1,11 @@
-set(CPACK_BUNDLE_NAME "XMoto")
+set(CPACK_BUNDLE_NAME "${APP_NAME}")
+set(CPACK_BUNDLE_PLIST "${PROJECT_SOURCE_DIR}/macos/Info.plist")
+set(CPACK_BUNDLE_ICON "${PROJECT_SOURCE_DIR}/extra/X-Moto.icns")
+set(CPACK_PACKAGE_ICON "${CPACK_BUNDLE_ICON}")
 
 set(MACOS_PLIST_PATH "${CMAKE_BINARY_DIR}/macos/Info.plist")
 set(MACOS_PLIST_IN_PATH "${PROJECT_SOURCE_DIR}/macos/Info.plist.in")
 
-set(CPACK_BUNDLE_ICON "${PROJECT_SOURCE_DIR}/macos/xmoto.icns")
 set(CPACK_DMG_VOLUME_NAME "${APP_NAME} ${APP_VERSION}")
 
 include(InstallRequiredSystemLibraries)
@@ -12,9 +14,12 @@ if(BUILD_MACOS_BUNDLE)
   configure_file("${MACOS_PLIST_IN_PATH}" "${MACOS_PLIST_PATH}")
 
   set_target_properties(xmoto PROPERTIES
+    OUTPUT_NAME "${CPACK_BUNDLE_NAME}"
+
     MACOSX_BUNDLE_INFO_PLIST
     "${MACOS_PLIST_PATH}"
   )
+
   install(TARGETS xmoto
     RUNTIME DESTINATION . COMPONENT Runtime
     BUNDLE DESTINATION . COMPONENT Runtime
@@ -22,6 +27,6 @@ if(BUILD_MACOS_BUNDLE)
 
   install(CODE "
     include(BundleUtilities)
-    fixup_bundle(\"\${CMAKE_INSTALL_PREFIX}/xmoto.app\"  \"\"  \"\")
+    fixup_bundle(\"\${CMAKE_INSTALL_PREFIX}/${CPACK_BUNDLE_NAME}.app\"  \"\"  \"\")
     " COMPONENT Runtime)
 endif()
