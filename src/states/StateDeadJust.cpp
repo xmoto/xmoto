586374b996f2a60a9ab7c0adaefe2b14ce487e98
_yui <imbatman0xff@gmail.com>
2023-03-03 09:57:50 +0200

    Improve safety of safemode
    When safemode is enabled, only allow quick restart in death screen if no checkpoint has been reached

--- src/states/StateDeadJust.cpp
+++ src/states/StateDeadJust.cpp
@@ -47,36 +47,46 @@ void StateDeadJust::enter() {
   StateScene::enter();
   m_enterTime = 0;
 
-  if (m_universe != NULL) {
-    for (unsigned int i = 0; i < m_universe->getScenes().size(); i++) {
-      m_universe->getScenes()[i]->clearGameMessages();
-      m_universe->getScenes()[i]->setInfos(
-        m_universe->getScenes()[i]->getLevelSrc()->Name());
-    }
-    m_universe->getScenes()[0]->gameMessage(
-      (m_universe->getScenes()[0]->getCheckpoint() != NULL
-         ? GAMETEXT_JUSTDEAD_CHECKPOINT
-         : std::string("")) +
-        std::string("\n") + GAMETEXT_JUSTDEAD_RESTART + std::string("\n") +
-        GAMETEXT_JUSTDEAD_DISPLAYMENU,
-      true,
-      260,
-      gameMsg); // in multiplayer its good, to have it displayed only once
+  if (!m_universe) {
+    return;
   }
 
-  if (m_universe != NULL) {
-    if (m_universe->getScenes().size() == 1) {
-      if (SendVoteThread::isToPropose(
-            xmDatabase::instance("main"),
-            m_universe->getScenes()[0]->getLevelSrc()->Id())) {
-        StateManager::instance()->pushState(
-          new StateVote(m_universe->getScenes()[0]->getLevelSrc()->Id()));
-      }
+  bool safemode = XMSession::instance()->isSafemodeActive();
+
+  for (unsigned int i = 0; i < m_universe->getScenes().size(); i++) {
+    m_universe->getScenes()[i]->clearGameMessages();
+    m_universe->getScenes()[i]->setInfos(
+      m_universe->getScenes()[i]->getLevelSrc()->Name());
+  }
+
+  std::string message;
+
+  if (checkpointReached()) {
+    message = GAMETEXT_JUSTDEAD_CHECKPOINT;
+  }
+
+  if (!safemode || !checkpointReached()) {
+    message += "\n" + std::string(GAMETEXT_JUSTDEAD_RESTART);
+  }
+
+  message += "\n" + std::string(GAMETEXT_JUSTDEAD_DISPLAYMENU);
+
+  // in multiplayer its good, to have it displayed only once
+  m_universe->getScenes()[0]->gameMessage(message, true, 260, gameMsg);
+
+  if (m_universe->getScenes().size() == 1) {
+    if (SendVoteThread::isToPropose(
+          xmDatabase::instance("main"),
+          m_universe->getScenes()[0]->getLevelSrc()->Id())) {
+      StateManager::instance()->pushState(
+        new StateVote(m_universe->getScenes()[0]->getLevelSrc()->Id()));
     }
   }
 }
 
 void StateDeadJust::xmKey(InputEventType i_type, const XMKey &i_xmkey) {
+  bool safemode = XMSession::instance()->isSafemodeActive();
+
   if (i_type == INPUT_DOWN && (i_xmkey == XMKey(SDLK_ESCAPE, KMOD_NONE) ||
                                i_xmkey.getJoyButton() == SDL_CONTROLLER_BUTTON_START)) {
     StateManager::instance()->pushState(
@@ -86,8 +96,10 @@ void StateDeadJust::xmKey(InputEventType i_type, const XMKey &i_xmkey) {
   else if (i_type == INPUT_DOWN &&
            i_xmkey ==
              (*Input::instance()->getGlobalKey(INPUT_RESTARTLEVEL))) {
-    /* retart immediatly the level */
-    restartLevel();
+    if (!safemode || !checkpointReached()) {
+      /* restart the level immediately */
+      restartLevel();
+    }
   }
 
   else if (i_type == INPUT_DOWN &&
@@ -112,13 +124,13 @@ void StateDeadJust::nextLevel(bool i_positifOrder) {
 bool StateDeadJust::update() {
   float v_torsoVelocity = 0.0;
 
-  if (XMSession::instance()->enableDeadAnimation() == false) {
+  if (!XMSession::instance()->enableDeadAnimation()) {
     StateManager::instance()->pushState(
       new StateDeadMenu(m_universe, getStateId()));
     return false;
   }
 
-  if (m_universe == NULL) {
+  if (!m_universe) {
     return false;
   }
 
@@ -220,4 +232,8 @@ void StateDeadJust::toCheckpointBeeingDead() {
     StateManager::instance()->replaceState(
       new StatePlayingLocal(m_universe, m_renderer), getStateId());
   }
+}
+
+bool StateDeadJust::checkpointReached() const {
+ return m_universe->getScenes()[0]->getCheckpoint() != nullptr;
 }
