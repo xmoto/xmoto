0035a4873944e0d2341e29707b30f0c033825116
_yui <imbatman0xff@gmail.com>
2023-03-05 07:45:15 +0200

    CMake: Configure version string for build config

--- src/common/svn_version.h
+++ src/common/svn_version.h
@@ -21,6 +21,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #ifndef __SVNVERSION_H__
 #define __SVNVERSION_H__
 
+#include <string>
+
 std::string svn_version();
 
 #endif
