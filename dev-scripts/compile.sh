e421eba1665167ee8e8f2bbfa3e39f7bdc448501
AnneKitsune <annekitsunefox@gmail.com>
2025-04-13 21:33:51 -0400

    switch to ninja for the dev script compile.

--- dev-scripts/compile.sh
+++ dev-scripts/compile.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 # Run this from the root directory. Will compile and install a debug build.
+# Requires ninja.
 
 rm -r /tmp/xm
-mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_INSTALL_PREFIX=/tmp/xm ..
+cmake -B build -S . -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_INSTALL_PREFIX=/tmp/xm -G Ninja
 THREAD_COUNT="$(( $(nproc) - 2 > 0 ? $(nproc) - 2 : 1 ))"
-make -j$THREAD_COUNT
-make install
+CMAKE_BUILD_PARALLEL_LEVEL="$THREAD_COUNT" cmake --build build --target all
+cmake --build build --target install
